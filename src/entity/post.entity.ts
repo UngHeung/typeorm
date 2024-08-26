d3e3b915964aabbf04d53939542be1a94d9789ab
ungheung <t01047377477@gmail.com>
2024-08-27 01:10:28 +0900

    fix : JoinColumn -> JoinTable 데코레이터 변경

--- src/entity/post.entity.ts
+++ src/entity/post.entity.ts
@@ -1,7 +1,7 @@
 import {
   Column,
   Entity,
-  JoinColumn,
+  JoinTable,
   ManyToMany,
   ManyToOne,
   PrimaryGeneratedColumn,
@@ -18,10 +18,10 @@ export class PostModel {
   title: string;
 
   // 상대에 관련된 컬럼을 가지는 쪽은 Many쪽이다. DB Table 특성상 여러개의 값을 가질 수 없기 때문
-  @ManyToOne(() => UserModel, (user) => user.posts, { eager: false })
+  @ManyToOne(() => UserModel, (user) => user.posts)
   author: UserModel;
 
   @ManyToMany(() => TagModel, (tag) => tag.posts)
-  @JoinColumn()
+  @JoinTable()
   tags: TagModel[];
 }
